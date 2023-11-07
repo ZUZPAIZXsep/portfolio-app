74b6b5e716fd15728d9908362ced0d719780d653
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-11-07 17:28:07 +0700

    update2

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,9 +11,9 @@ function Home() {
         <div className="col-md-6">
           <img src="home-pic.jpg" alt="homepic" className="img-fluid my-3 img-small img-with-border rounded" />
         </div>
-        <div className="col-md-6">
-          <h2 className="mb-2">กันทรากร ตั้งกิจจาวิสุทธิ์</h2>
-          <p>มหาวิยาลัย</p>
+        <div className="col-md-5 my-3 text-color-background rounded">
+          <h2 className="my-2 text-color ">กันทรากร ตั้งกิจจาวิสุทธิ์</h2>
+          <h3 className="my-5 text-color">มหาวิทยาลัย</h3>
           <p>คณะ</p>
         </div>
       </div>
