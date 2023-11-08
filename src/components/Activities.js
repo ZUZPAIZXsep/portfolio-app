168943d809032d0db586ee60ed44c65ad1ac91df
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-11-08 14:03:48 +0700

    success all
    success all

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {FaHome} from 'react-icons/fa';
-import { BiSolidLeftArrow} from "react-icons/bi";
+import { FaHotel,FaHome,} from 'react-icons/fa';
+import { BiSolidLeftArrow } from "react-icons/bi";
+import { SiBitcoincash } from "react-icons/si";
 import './styles.css';
-
 function Activities() {
   return (
-    <div className="container">
-      <h1 className="my-5">Activities</h1>
-      {/* เพิ่มรายการกิจกรรมที่เคยทำที่นี่ */}
-      <Link to="/about" className="btn btn-primary mx-2"><BiSolidLeftArrow /> หน้าก่อนหน้า</Link>
-      <Link to="/home" className="btn btn-secondary mx-2"><FaHome /> หน้าหลัก</Link>
+    <div className="container about rounded">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className='thai-text text-color'>ACTIVITIES</h1>
+          <p className='aboutme rounded'>
+            <h2 className='thai-text'><FaHotel /> HOTEL RESERVATION WEB-APPLICATION</h2>
+            <h4 className='thai-text'>ผลงานการสร้างเว็บจองโรงแรมที่จัดทำขึ้นในวิชา Software Engineering</h4>
+            <img src="hotelweb.png" alt="hotelpic" style={{ borderRadius: '5px' }} className="img-fluid mt-2 rounded"/>
+            <img src="hotelweb2.png" alt="hotelpic2" style={{ borderRadius: '5px' }} className="img-fluid mt-3 rounded"/>
+          </p>
+          <p className='contactme rounded'>
+          <h2 className='thai-text'><SiBitcoincash /> CURRENCY EXCHANGE API</h2>
+            <h4 className='thai-text'>ผลงานการสร้าง api เว็บแลกเปลี่ยนสกุลเงินจัดทำขึ้นในวิชา Mobile & Cloud Computing</h4>
+            <img src="apiexchange.png" alt="apipic" style={{ borderRadius: '5px' }} className="img-fluid mt-2 rounded"/>
+            <img src="apiexchange2.png" alt="apipic2" style={{ borderRadius: '5px' }} className="img-fluid mt-3 rounded"/>
+            <img src="apiexchange3.png" alt="apipic3" style={{ borderRadius: '5px' }} className="img-fluid mt-3 rounded"/>
+            <img src="apiexchange6.png" alt="apipic6" style={{ borderRadius: '5px' }} className="img-fluid mt-3 rounded"/>
+            <img src="apiexchange4.png" alt="apipic4" style={{ borderRadius: '5px' }} className="img-fluid mt-3 rounded"/>
+            <img src="apiexchange5.png" alt="apipic5" style={{ borderRadius: '5px' }} className="img-fluid mt-3 rounded"/>
+          </p>
+          <Link to="/home" className="btn btn-secondary mx-2"><FaHome /> หน้าหลัก</Link>
+          <Link to="/about" className="btn btn-primary mx-2 "><BiSolidLeftArrow /> หน้าก่อนหน้า</Link>
+        </div>
+      </div>
     </div>
   );
 }
